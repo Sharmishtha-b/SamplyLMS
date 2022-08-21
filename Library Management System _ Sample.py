036104e8bf145632efa03c4061d360311b76105e
Sharmishtha Bharti <42813234+Sharmishtha-b@users.noreply.github.com>
2022-08-21 12:41:13 +0530

    Add files via upload

---
+++ Library Management System _ Sample.py
@@ -0,0 +1,96 @@
+-"""
+Date: 29 March 2022
+Author: Sharmishtha Bharti and Ananya Krishna Preetha
+Project: Student library management system
+"""
+
+
+class Library:
+    def __init__(self, listofBooks):
+        self.books = listofBooks
+
+    def displayAvailableBooks(self):
+        print(f"\n{len(self.books)} THE AVAILABLE BOOKS ARE: ")
+        for book in self.books:
+            print(" ♦-- " + book)
+        print("\n")
+
+    def borrowBook(self, name, bookname):
+        if bookname not in self.books:
+            print(
+                f"{bookname} THE BOOK IS NOT AVAILABLE AS IT IS TAKEN BY SOMEONE ELSE. PLEASE WAIT UNTIL THE BOOK IS RETURNED.\n")
+        else:
+            track.append({name: bookname})
+            print("BOOK IS ISSUED. KINDLY HANDLE WITH CARE AND RETURN IT ON TIME. THANK YOU!.\n")
+            self.books.remove(bookname)
+
+    def returnBook(self, bookname):
+        print("BOOK RETURNED : THANK YOU! \n")
+        self.books.append(bookname)
+
+    def donateBook(self, bookname):
+        print("BOOK DONATED : THANK YOU VERY MUCH, HAVE A GREAT DAY AHEAD !.\n")
+        self.books.append(bookname)
+
+
+class Student():
+    def requestBook(self):
+        print("Borrow a book: ")
+        self.book = input("Enter name of the book you want to borrow: ")
+        return self.book
+
+    def returnBook(self):
+        print("Return the Book: ")
+        name = input("Enter your name: ")
+        self.book = input("Enter name of the book you want to return: ")
+        if {name: self.book} in track:
+            track.remove({name: self.book})
+        return self.book
+
+    def donateBook(self):
+        print("Donate a book: ")
+        self.book = input("Enter name of the book you want to donate: ")
+        return self.book
+
+
+if __name__ == "__main__":
+
+    Delhilibrary = Library(
+        ["vistas", "invention", "rich&poor", "indian", "macroeconomics", "microeconomics"])
+    student = Student()
+    track = []
+
+    print("\t\t\t\t\t\t\t------ WELCOME TO THE LIBRARY -------\n")
+    print("""CHOOSE WHAT YOU WANT TO DO:-\n1. Listing all books\n2. Borrow books\n3. Return books\n4. Donate books\n5. Track books\n6. exit the library\n""")
+
+    while (True):
+        # print(track)
+        try:
+            usr_response = int(input("Enter your choice: "))
+
+            if usr_response == 1:  # listing
+                Delhilibrary.displayAvailableBooks()
+            elif usr_response == 2:  # borrow
+                Delhilibrary.borrowBook(
+                    input("Enter your name: "), student.requestBook())
+            elif usr_response == 3:  # return
+                Delhilibrary.returnBook(student.returnBook())
+            elif usr_response == 4:  # donate
+                Delhilibrary.donateBook(student.donateBook())
+            elif usr_response == 5:  # track
+                for i in track:
+                    for key, value in i.items():
+                        holder = key
+                        book = value
+                        print(f"{book} This Book is already issued by {holder}.")
+                print("\n")
+                if len(track) == 0:
+                    print("NO BOOKS ARE ISSUED!. \n")
+            
+            elif usr_response == 6: #exit
+                print("THANK YOU ! \n")
+                exit()
+            else:
+                print("INVAILD INPUT! \n")
+        except Exception as e:              #catch errors
+            print(f"{e}---> INVALID INPUT! \n")
